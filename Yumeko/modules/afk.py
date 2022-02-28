7be7be405ea154edf0f8a63a8cd51e417d087a5f
Voidxtoxic <97348572+Voidxtoxic@users.noreply.github.com>
2022-02-28 08:15:38 +0530

    Update afk.py

--- Yumeko/modules/afk.py
+++ Yumeko/modules/afk.py
@@ -58,14 +58,8 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "{} is baka! [ ](https://telegra.ph/file/78b300985668e3ce37ec9.jpg) "
+                
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
