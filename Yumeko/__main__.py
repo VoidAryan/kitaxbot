c4534bee703de11791e114bbeb20ddb6eee58680
Voidxtoxic <97348572+Voidxtoxic@users.noreply.github.com>
2022-02-28 07:44:44 +0530

    Update __main__.py

--- Yumeko/__main__.py
+++ Yumeko/__main__.py
@@ -74,8 +74,7 @@ def get_readable_time(seconds: int) -> str:
 
 YUMEKO_IMG = "https://telegra.ph/file/78b300985668e3ce37ec9.jpg"
 
-PM_START_TEXT = """[](https://telegra.ph/file/78b300985668e3ce37ec9.jpg)
-Hello There !
+PM_START_TEXT = """Hello There ![](https://telegra.ph/file/78b300985668e3ce37ec9.jpg)
 ✪ I'm an anime-theme management bot ✨
 ────────────────────────
 • Kita's Darling: @Achillues
